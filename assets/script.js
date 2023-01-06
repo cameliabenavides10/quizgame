d4f35962be2b7e9211620a3be1f36705aeac2b34
cameliabenavides10 <cameliabenavides@gmail.com>
2023-01-06 16:05:14 -0600

    added html

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-var score= document.querySelector(".score");
+var score = document.querySelector(".score");
 var scoreEl = document.createElement("div");
 scoreEl.textContent = "Highscore";
 score.append(scoreEl);
 
-
+var secsLong = 60;
 // you select the part of the document
-var timer= document.querySelector(".timer");
+var timer = document.querySelector(".timer");
 // you create somewhere to put it
 var timerEl = document.createElement("div");
 // you set what you want the content to say
-timerEl.textContent = "Timer: 60";
+timerEl.textContent = "Timer:" + secsLong;
 // you attach what you created to the part of the document you selected
 timer.append(timerEl);
 
@@ -17,7 +17,7 @@ timer.append(timerEl);
 
 
 
-var currentQuestion= 0;
+var currentQuestion = 0;
 
 var quizContent = [
     {
@@ -31,9 +31,9 @@ var quizContent = [
         correctAnswer: "blue"
     },
     {
-        questions: "what is my name?",
+        question: "what is my name?",
         answers: ["Abby", "poop", "waaaa", "ugh"],
-        correntAnswer: "Abby"
+        correctAnswer: "Abby"
 
     }
 ]
@@ -42,67 +42,66 @@ var quizContent = [
 
 
 
-// click event to get to next questions
-// currentQuestion++;
+
+function displayQuestions() {
+    var questions = document.querySelector(".menu");
+    var questionsEl = document.createElement("h1");
+    questions.innerHTML = "";
+    questionsEl.innerHTML = "";
+    questionsEl.textContent = quizContent[currentQuestion].question;
+    questions.append(questionsEl);
+
+    // for loop to get the answers from the array into the document 
+    for (var i = 0; i < quizContent[currentQuestion].answers.length; i++) {
+        var indAnswers = (quizContent[currentQuestion].answers[i]);
+        console.log(indAnswers);
+        var answer = document.querySelector(".menu");
+        var answerEl = document.createElement("li");
+        answerEl.textContent = indAnswers;
+        answerEl.setAttribute("type", "button");
+        answerEl.setAttribute("Style", "border: 1px solid; max-width: 30%; margin: 5px auto; border-radius: 5px");
+        answerEl.addEventListener("click", function (event) {
+            console.log(indAnswers);
+            console.log((quizContent[currentQuestion].correctAnswer));
+            if ((event.target.textContent) === (quizContent[currentQuestion].correctAnswer)) {
+                alert("Correct Answer!")
+            } else {
+                secsLong -= 10;
+                alert("Wrong Answer!");
+            }
 
 
-var questions = document.querySelector(".menu");
-var questionsEl = document.createElement("h1");
-questionsEl.textContent = quizContent[currentQuestion].question;
-questions.append(questionsEl);
-
-
-
-
-// for loop to get the answers from the array into the document 
-for (var i = 0; i < quizContent[currentQuestion].answers.length; i++) {
-    var indAnswers = (quizContent[currentQuestion].answers[i]);
-    console.log(indAnswers);
-var answer = document.querySelector(".menu");
-var answerEl = document.createElement("li");
-answerEl.textContent = indAnswers;
-answerEl.setAttribute("type", "button");
-answerEl.setAttribute("Style", "border: 1px solid; max-width: 30%; margin: 5px auto; border-radius: 5px");
-answer.appendChild(answerEl);
+            if (currentQuestion < quizContent.length-1) {
+                currentQuestion++;
+                displayQuestions();
+            } else{
+                location.href= "highscore.html?score=" + secsLong
+            }
+        })
+        answer.appendChild(answerEl);
+    }
 }
-
-
-// console.log(quizContent[currentQuestion].question);
-
-// console.log(questions[currentQuestion].correctAnswer);
-
-// for (var i = 0; i < quizContent[currentQuestion].answers.length; i++) {
-    // console.log(quizContent[currentQuestion].answers[i]); }
-
-
-// nextPage.addEventListener("click", function(){
+displayQuestions();
 
 
 
 
 
-// }
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-// function setTime() {
-    // timerInterval = setInterval(function() {
-        // secondsLeft--;
-        // timerCountdown.textContent = "Timer:" + secondsLeft
-   /     if(secondsLeft === 0) {
+function setTime() {
+    timerInterval = setInterval(function() {
+        secsLong--;
+        timer.innerHTML= "Timer: " + secsLong
+        if(secsLong === 0) {
             // Stops execution of action at set interval
-            // clearInterval(timerInterval);
-            // endGame();
-        // };
- //   }, 1000);
-// };
+         clearInterval(timerInterval);
+         location.href= "highscore.html?score=" + secsLong
+        };
+   }, 1000);
+ };
+ setTime();
+
+
+
+//  promt:enter INitials
+// save initials to local storage and score. 
+// retireve from local storgae and display the scores using a for loop. 
